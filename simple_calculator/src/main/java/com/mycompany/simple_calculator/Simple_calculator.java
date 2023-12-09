75bf7653ab94a30f9c947fa49ecb22f406090e70
Hasan Chinthaka <pgrhasanchinthaka@gmail.com>
2023-12-09 16:43:32 +0530

    Create simple cal app

---
+++ simple_calculator/src/main/java/com/mycompany/simple_calculator/Simple_calculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.simple_calculator;
+
+/**
+ *
+ * @author User
+ */
+public class Simple_calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
